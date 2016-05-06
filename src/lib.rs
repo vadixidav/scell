e40f22b2f1b62d13a2b67e9f649b33b2a50d9114
Geordon Worley <vadixidav@gmail.com>
2016-05-06 14:50:52 -0400

    reduce codebase and make comments better

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,85 @@ pub use checked::*;
 mod unchecked;
 #[cfg(feature = "unchecked")]
 pub use unchecked::*;
+
+use std::cmp::Ordering;
+use std::fmt::{Formatter, Display, Debug, Error, Pointer};
+use std::hash::{Hasher, Hash};
+
+impl<T: ?Sized> PartialEq for SCell<T>
+    where T: PartialEq
+{
+    fn eq(&self, other: &Self) -> bool {
+        *self.borrow() == *other.borrow()
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        *self.borrow() != *other.borrow()
+    }
+}
+
+impl<T: ?Sized> Eq for SCell<T> where T: Eq {}
+
+impl<T: ?Sized> PartialOrd for SCell<T>
+    where T: PartialOrd
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.borrow().partial_cmp(&*other.borrow())
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        *self.borrow() < *other.borrow()
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        *self.borrow() <= *other.borrow()
+    }
+
+    fn gt(&self, other: &Self) -> bool {
+        *self.borrow() > *other.borrow()
+    }
+
+    fn ge(&self, other: &Self) -> bool {
+        *self.borrow() >= *other.borrow()
+    }
+}
+
+impl<T: ?Sized> Ord for SCell<T>
+    where T: Ord
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.borrow().cmp(&*other.borrow())
+    }
+}
+
+impl<T: ?Sized> Hash for SCell<T>
+    where T: Hash
+{
+    fn hash<H>(&self, state: &mut H)
+        where H: Hasher
+    {
+        self.borrow().hash(state);
+    }
+}
+
+impl<T: ?Sized> Display for SCell<T>
+    where T: Display
+{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        self.borrow().fmt(f)
+    }
+}
+
+impl<T: ?Sized> Debug for SCell<T>
+    where T: Debug
+{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        self.borrow().fmt(f)
+    }
+}
+
+impl<T> From<T> for SCell<T> {
+    fn from(t: T) -> Self {
+        SCell::new(t)
+    }
+}
--- src/unchecked.rs
+++ src/unchecked.rs
@@ -1,11 +1,11 @@
 use std::rc::Rc;
 use std::cell::UnsafeCell;
 use std::ops::{Deref, DerefMut};
-use std::cmp::Ordering;
 use std::fmt::{Formatter, Display, Debug, Error, Pointer};
-use std::hash::{Hasher, Hash};
 
 /// A smart container for objects in recursive data structures
+///
+/// This container contains Rc and therefore `clone()` will create a new reference to the same instance.
 #[derive(Default)]
 pub struct SCell<T: ?Sized>(Rc<UnsafeCell<T>>);
 
@@ -16,17 +16,17 @@ pub struct Ref<'a, T: 'a + ?Sized>(&'a T);
 pub struct RefMut<'a, T: 'a + ?Sized>(&'a mut T);
 
 impl<T> SCell<T> {
-    fn new(t: T) -> Self {
+    pub fn new(t: T) -> Self {
         SCell(Rc::new(UnsafeCell::new(t)))
     }
 }
 
 impl<T: ?Sized> SCell<T> {
-    fn borrow(&self) -> Ref<T> {
+    pub fn borrow(&self) -> Ref<T> {
         Ref(unsafe{&*self.0.get() as &T})
     }
 
-    fn borrow_mut(&self) -> RefMut<T> {
+    pub fn borrow_mut(&self) -> RefMut<T> {
         RefMut(unsafe{&mut *self.0.get() as &mut T})
     }
 }
@@ -37,87 +37,9 @@ impl<T: ?Sized> Clone for SCell<T> {
     }
 }
 
-impl<T: ?Sized> PartialEq for SCell<T>
-    where T: PartialEq
-{
-    fn eq(&self, other: &Self) -> bool {
-        *self.borrow() == *other.borrow()
-    }
-
-    fn ne(&self, other: &Self) -> bool {
-        *self.borrow() != *other.borrow()
-    }
-}
-
-impl<T: ?Sized> Eq for SCell<T> where T: Eq {}
-
-impl<T: ?Sized> PartialOrd for SCell<T>
-    where T: PartialOrd
-{
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.borrow().partial_cmp(&*other.borrow())
-    }
-
-    fn lt(&self, other: &Self) -> bool {
-        *self.borrow() < *other.borrow()
-    }
-
-    fn le(&self, other: &Self) -> bool {
-        *self.borrow() <= *other.borrow()
-    }
-
-    fn gt(&self, other: &Self) -> bool {
-        *self.borrow() > *other.borrow()
-    }
-
-    fn ge(&self, other: &Self) -> bool {
-        *self.borrow() >= *other.borrow()
-    }
-}
-
-impl<T: ?Sized> Ord for SCell<T>
-    where T: Ord
-{
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.borrow().cmp(&*other.borrow())
-    }
-}
-
-impl<T: ?Sized> Hash for SCell<T>
-    where T: Hash
-{
-    fn hash<H>(&self, state: &mut H)
-        where H: Hasher
-    {
-        self.borrow().hash(state);
-    }
-}
-
-impl<T: ?Sized> Display for SCell<T>
-    where T: Display
-{
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        self.borrow().fmt(f)
-    }
-}
-
-impl<T: ?Sized> Debug for SCell<T>
-    where T: Debug
-{
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        self.borrow().fmt(f)
-    }
-}
-
 impl<T: ?Sized> Pointer for SCell<T> {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         self.0.fmt(f)
-    }
-}
-
-impl<T> From<T> for SCell<T> {
-    fn from(t: T) -> Self {
-        SCell::new(t)
     }
 }
 
